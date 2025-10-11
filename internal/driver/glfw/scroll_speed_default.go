a5891a35e84a1b5512e31ba3b2f260fd03d6ccd7
Drew Weymouth <dweymouth@gmail.com>
2025-10-11 08:43:04 -0700

    increase scrolling speed

--- internal/driver/glfw/scroll_speed_default.go
+++ internal/driver/glfw/scroll_speed_default.go
@@ -3,7 +3,7 @@
 package glfw
 
 const (
-	scrollAccelerateRate   = float64(125)
+	scrollAccelerateRate   = float64(200)
 	scrollAccelerateCutoff = float64(10)
-	scrollSpeed            = float32(25)
+	scrollSpeed            = float32(75)
 )
