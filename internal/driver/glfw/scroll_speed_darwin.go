56844b52fd2cb1d89169650fb05e88905da13a60
Drew Weymouth <dweymouth@users.noreply.github.com>
2026-06-29 16:49:58 -0700

    Supersonic base 2.7.x (#4)
    * add custom SetOnMouseBack/Forward APIs
    
    * temporary fix for Fyne issue 4374
    
    * increase scrolling speed
    
    * be extra sure we won't leak cursor animations

--- internal/driver/glfw/scroll_speed_darwin.go
+++ internal/driver/glfw/scroll_speed_darwin.go
@@ -7,5 +7,5 @@ const (
 	// scrollAccelerateRate to 1 for no acceleration effect
 	scrollAccelerateRate   = float64(1)
 	scrollAccelerateCutoff = float64(5)
-	scrollSpeed            = float32(10)
+	scrollSpeed            = float32(15)
 )
