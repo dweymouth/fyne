275152d99e69181e92aa5a1ff00c94d04427d705
Drew Weymouth <dweymouth@gmail.com>
2026-08-26 15:36:10 -0700

    deprecate fyne.Min and fyne.Max in favor of the Go builtins

--- math.go
+++ math.go
@@ -1,17 +1,15 @@
 package fyne
 
 // Min returns the smaller of the passed values.
+//
+// Deprecated: use the Go builtin min(x, y) instead
 func Min(x, y float32) float32 {
-	if x < y {
-		return x
-	}
-	return y
+	return min(x, y)
 }
 
 // Max returns the larger of the passed values.
+//
+// Deprecated: use the Go builtin max(x, y) instead
 func Max(x, y float32) float32 {
-	if x > y {
-		return x
-	}
-	return y
+	return max(x, y)
 }
