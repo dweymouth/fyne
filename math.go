91d250116d2ef412a70df6e64ddb8ecf531c2a18
Drew Weymouth <dweymouth@gmail.com>
2026-08-26 15:39:02 -0700

    add the go:fix directive for Min and Max

--- math.go
+++ math.go
@@ -3,6 +3,8 @@ package fyne
 // Min returns the smaller of the passed values.
 //
 // Deprecated: use the Go builtin min(x, y) instead
+//
+//go:fix inline
 func Min(x, y float32) float32 {
 	return min(x, y)
 }
@@ -10,6 +12,8 @@ func Min(x, y float32) float32 {
 // Max returns the larger of the passed values.
 //
 // Deprecated: use the Go builtin max(x, y) instead
+//
+//go:fix inline
 func Max(x, y float32) float32 {
 	return max(x, y)
 }
